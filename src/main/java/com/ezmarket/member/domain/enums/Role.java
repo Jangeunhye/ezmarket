17e05691abfb6af3485d1d33622b7d718f600e9a
Jangeunhye <dmsgp220@gmail.com>
2024-09-03 23:00:32 +0900

    feat: 회원가입 구현

---
+++ src/main/java/com/ezmarket/member/domain/enums/Role.java
@@ -0,0 +1,6 @@
+package com.ezmarket.member.domain.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/ezmarket/member/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.ezmarket.member.service;
+
+import com.ezmarket.member.domain.entity.Member;
+import com.ezmarket.member.dto.MemberDto;
+
+import com.ezmarket.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public MemberDto signUp(MemberDto memberDto){
+
+        Member member = memberDto.toEntity(passwordEncoder);
+
+        Member savedMember = memberRepository.save(member);
+
+        return MemberDto.ofEntity(savedMember);
+    }
+
+    public void checkDuplicateEmail(String email, BindingResult bindingResult){
+        boolean isDuplicated = memberRepository.existsByEmail(email);
+        if (isDuplicated){
+            bindingResult.addError(new FieldError("memberDto","email", "이미 가입된 이메일입니다"));
+        }
+    }
+}
