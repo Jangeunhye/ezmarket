7cab3b907bfacfe8ef7adb471a970828c3b3423a
Jangeunhye <dmsgp220@gmail.com>
2024-09-06 18:09:39 +0900

    feat: 세션 로그인 기능

--- src/main/java/com/ezmarket/MainController.java
+++ src/main/java/com/ezmarket/MainController.java
@@ -1,13 +1,17 @@
 package com.ezmarket;
 
+import com.ezmarket.member.dto.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class MainController {
 
+
     @GetMapping(value = "/")
-    public String main(){
+    public String main(@AuthenticationPrincipal CustomUserDetails customUserDetails){
         return "main";
     }
 }
