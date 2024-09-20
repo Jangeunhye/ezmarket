ca751db810abfca9152b2db99c0a1e8e63019983
Jangeunhye <dmsgp220@gmail.com>
2024-09-21 02:01:24 +0900

    feat: 권한별 상세페이지 및 상품관리페이지

--- src/main/java/com/ezmarket/item/repository/ItemRepository.java
+++ src/main/java/com/ezmarket/item/repository/ItemRepository.java
@@ -1,7 +1,11 @@
 package com.ezmarket.item.repository;
 
 import com.ezmarket.item.domain.entity.Item;
+import com.ezmarket.item.domain.enums.SellStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findBySellStatus(SellStatus sellStatus);
 }
